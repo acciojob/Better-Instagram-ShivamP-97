d419b7b4792e6fdd50ad209a0bae30edde2e5405
ShivamP-97 <shivampathak19972@gmail.com>
2025-10-13 23:28:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
 //your code here
+let draggedDiv = null;
+
+const imageDivs = document.querySelectorAll('.image');
+
+imageDivs.forEach((div) => {
+  div.addEventListener('dragstart', (e) => {
+    draggedDiv = e.target;
+    e.target.classList.add('selected'); 
+  });
+
+  div.addEventListener('dragend', (e) => {
+    e.target.classList.remove('selected');
+  });
+
+  div.addEventListener('dragover', (e) => {
+    e.preventDefault(); 
+  });
+
+  div.addEventListener('drop', (e) => {
+    e.preventDefault();
+
+    if (draggedDiv !== e.target) {
+      const temp = draggedDiv.style.backgroundImage;
+      draggedDiv.style.backgroundImage = e.target.style.backgroundImage;
+      e.target.style.backgroundImage = temp;
+    }
+  });
+});
