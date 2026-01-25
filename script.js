e5047f98b68470f18e954a0fe3f7600bde3c2d90
ShivamP-97 <shivampathak19972@gmail.com>
2026-01-25 22:08:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,25 +1,18 @@
-const images = document.querySelectorAll(".image");
-let draggedItem = null;
-images.forEach((image) => {
-  image.addEventListener("dragstart", (e) => {
-    draggedItem = e.target;
-    e.target.style.opacity = "0.5";
-  });
-  image.addEventListener("dragend", (e) => {
-    e.target.style.opacity = "1";
-    draggedItem = null;
-  });
-  image.addEventListener("dragover", (e) => {
-    e.preventDefault();
-  });
-  image.addEventListener("drop", (e) => {
-    e.preventDefault();
-    const dropTarget = e.target;
-    if (draggedItem !== dropTarget) {
-      const draggedItemBg = window.getComputedStyle(draggedItem).backgroundImage;
-      const dropTargetBg = window.getComputedStyle(dropTarget).backgroundImage;
-      draggedItem.style.backgroundImage = dropTargetBg;
-      dropTarget.style.backgroundImage = draggedItemBg;
-    }
-  });
-});
+let dragElement=null
+document.addEventListener("dragstart",(e)=>{
+	dragElement=e.target
+})
+document.addEventListener("dragover",(e)=>{
+	e.preventDefault();
+})
+document.addEventListener("drop",(e)=>{
+	e.preventDefault();
+	let target=e.target;
+
+	if(target!=dragElement && target.parentElement.id=="parent"){
+			let temp=document.createElement("div");
+			target.replaceWith(temp);
+			dragElement.replaceWith(target);
+			temp.replaceWith(dragElement);
+	}
+})
